c0603d941cff8e8fda44ff413fb1400480011b80
zagob <matheusbestana@gmail.com>
2023-01-17 18:46:54 -0300

    signIn google with next-aut + create adapter

---
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -0,0 +1,34 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "../../../lib/auth/prisma-adapter";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(),
+
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+      authorization: {
+        params: {
+          scope:
+            "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar",
+        },
+      },
+    }),
+  ],
+
+  callbacks: {
+    async signIn({ account }) {
+      if (
+        !account?.scope?.includes("https://www.googleapis.com/auth/calendar")
+      ) {
+        return "/connect-calendar/?error=permissions";
+      }
+
+      return true;
+    },
+  },
+};
+
+export default NextAuth(authOptions);
--- src/pages/connect-calendar.page.tsx
+++ src/pages/connect-calendar.page.tsx
@@ -1,12 +1,21 @@
+import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import { ArrowRight } from "phosphor-react";
+import { ArrowRight, Check } from "phosphor-react";
 import { Multistep } from "../components/Multistep";
 import { api } from "../lib/axios";
 
 export default function ConnectCalendar() {
+  const session = useSession();
   const router = useRouter();
 
+  const hasAuthError = !!router.query.error;
+  const isSignedIn = session.status === "authenticated";
+
   async function handleRegister() {}
+
+  async function handleConnectCalendar() {
+    await signIn("google");
+  }
 
   return (
     <div className="max-w-[572px] mx-auto mt-4 py-10">
@@ -28,17 +37,38 @@ export default function ConnectCalendar() {
           <span className="text-gray-100 font-medium text-base">
             Google Calendar
           </span>
-          <button
-            type="button"
-            className="bg-transparent border border-green-600 text-green-500 text-sm flex items-center gap-2 py-2 px-4 hover:bg-green-600 hover:text-gray-100 rounded-md"
-          >
-            Conectar
-            <ArrowRight />
-          </button>
+          {isSignedIn ? (
+            <button
+              type="button"
+              className="bg-transparent border border-green-600 text-green-500 text-sm flex items-center gap-2 py-2 px-4 rounded-md"
+              disabled
+            >
+              Conectado
+              <Check weight="fill" />
+            </button>
+          ) : (
+            <button
+              type="button"
+              className="bg-transparent border border-green-600 text-green-500 text-sm flex items-center gap-2 py-2 px-4 hover:bg-green-600 hover:text-gray-100 rounded-md"
+              onClick={handleConnectCalendar}
+            >
+              Conectar
+              <ArrowRight />
+            </button>
+          )}
         </div>
+
+        {hasAuthError && (
+          <span className="text-[#f75a68] mt-2 text-sm">
+            Falha ao se conectar ao Google, verifique se você habilitou as
+            premissões de acesso ao Google Calendar.
+          </span>
+        )}
+
         <button
           type="submit"
           className="text-gray-100 bg-green-600 rounded-md p-3 flex items-center justify-center gap-2 hover:brightness-125 transition-all"
+          disabled={!isSignedIn}
         >
           Próximo passo <ArrowRight />
         </button>
